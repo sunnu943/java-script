e0c011122b8db6c70aff78e9faa2fcf7aa61406a
Sunnu <sunnukatari@gmail.com>
2026-03-13 10:16:10 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,11 @@
+let number=3;
+if(number>5){
+    console.log("number is greater than 5")
+
+}
+else if(number>2){
+    console.log("number is greater than 2")
+}
+else{
+    console.log("number is very less")
+}
